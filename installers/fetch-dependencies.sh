9306b69beb742ee2124b67c115be5e9e300d09cb
Tommaso Murolo <tommaso.murolo@datakrypto.com>
2025-05-17 16:10:30 +0200

    - added fetch-dependencies.sh

---
+++ installers/fetch-dependencies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This command must be run as root." >&2
+   exit 1
+fi
+
+set -e
+
+apt update -y
+apt install libyaml-dev cmake -y
+
+# Install CYaml
+git clone https://github.com/tlsa/libcyaml.git
+cd libcyaml
+
+make
+make install
+
+# Install zig
+curl -O https://ziglang.org/builds/zig-linux-x86_64-0.15.0-dev.552+bc2f7c754.tar.xz
+tar -xf zig-linux-x86_64-0.15.0-dev.552+bc2f7c754.tar.xz
+
+mv zig-linux-x86_64-0.15.0-dev.552+bc2f7c754 /usr/local/zig
+ln -s /usr/local/zig/zig /usr/local/bin/zig
+
+rm zig-linux-x86_64-0.15.0-dev.552+bc2f7c754.tar.xz
+rm -rf zig-linux-x86_64-0.15.0-dev.552+bc2f7c754
